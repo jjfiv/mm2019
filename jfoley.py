1075cb8e04964f3b22a525b48a170b56b74aedab
John Foley <jjfoley@smith.edu>
2019-08-07 14:24:32 -0400

    edit formatting for #1

--- jfoley.py
+++ jfoley.py
@@ -3,9 +3,7 @@ def print_in_block(message):
   """
   print_in_block prints a message fancy
   """
-  print("="*40)
-  print(message)
-  print("="*40)
+  print("="*4, message)
 
 print_in_block("knock knock!")
 print_in_block("who's there?")
