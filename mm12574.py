e9f9815444eb7d5a078806b51d6064ba5cbe23a9
Trang Tran <tran_m4@denison.edu>
2019-08-07 11:39:28 -0400

    my updated file

--- mm12574.py
+++ mm12574.py
@@ -4,20 +4,17 @@ from typing import List
 # This is the main program:
 
 
-#def main(args: List[str]):
-def main():
-    print("This is my script")
+def main(args: List[str]):
+#loop through main arguments
+for arg in args:
+   print("You said: {0}".format(arg))
 
+   """
+   prints a message fancy
+   """
 
-
-
-
-  # loop through main arguments
- # for arg in args:
-#    print("You said: {0}".format(arg))
-
-  # or else print instructions
-#    print("Try this script with arguments!")
+  or else print instructions
+    print("Try this script with arguments!")
 
 if __name__ == '__main__':
   main(sys.argv[1:])
