38880d421989b8931c1c513c3067026130c760dd
omcomeau <ocomeau45@massmutual.com>
2019-08-07 13:55:35 -0400

    testing

--- emando.py
+++ emando.py
@@ -5,6 +5,7 @@ print("changing this to cause merge conflict")
 # This is the main program:
 def main():
     print("i am editing at the top")
+    print('PUT YOUR ANSWER IN QUOTES PLS')
     user_input = input("Congrats!!! you went to the gym! how do you feel?")
     print('hi there')
     print("i think we're going to have a merge conflict")
@@ -12,11 +13,10 @@ def main():
     print("What will happen")
     print("No clue")
     print("Hope I typed in the correct spot")
+    print(user_input)
 
 
 if __name__ == '__main__':
   main()
 
 print("This is a win")
-
-print(user_input)
