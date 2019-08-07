9ff680072d7de451d523d0582eb1032a3b92446a
Trang Tran <tran_m4@denison.edu>
2019-08-07 13:43:46 -0400

    Deleted

--- ebrice60.py
+++ ebrice60.py
@@ -3,7 +3,6 @@ import sys
 # This is the main program:
 def main():
     print("This my domain. Please knock when making edits. Thanks.")
-    print("This is my script!")
     print("Okay, now it's time for a rebase!")
 
 if __name__ == '__main__':
