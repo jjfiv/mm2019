9f773692a22d813b35a75a8887cca6cd5fb66695
omcomeau <ocomeau45@massmutual.com>
2019-08-07 13:36:43 -0400

    i made changes

--- emando.py
+++ emando.py
@@ -2,6 +2,8 @@ import sys
 
 # This is the main program:
 def main():
+    print("i am editing at the top")
+    user_input = input("Congrats!!! you went to the gym! how do you feel?")
     print('hi there')
     print("i think we're going to have a merge conflict")
     print("maybe we don't")
