0fb1f181114fc59bc772eca611c7d8a28d8f6fe7
John Foley <jjfoley@smith.edu>
2019-08-07 14:42:18 -0400

    revert to 2b4c882a55d6fe699268735f83aa30704793ecbf

--- jfoley.py
+++ jfoley.py
@@ -1,14 +1,3 @@
-
-def print_in_block(message):
-  """
-  print_in_block prints a message fancy
-  """
-  print("="*40)
-  print(message)
-  print("="*40)
-
-print_in_block("knock knock!")
-print_in_block("who's there?")
-print_in_block("interrupted cow")
-print_in_block("interrupted--")
-print_in_block("MOO!")
+print("="*40)
+print("Hello World!")
+print("="*40)
