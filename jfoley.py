e00a2f3ce940743f73fa7d0e1ed36b1caf4942fc
John Foley <jjfoley@smith.edu>
2019-08-07 14:16:08 -0400

    finish joke #1

--- jfoley.py
+++ jfoley.py
@@ -9,3 +9,6 @@ def print_in_block(message):
 
 print_in_block("knock knock!")
 print_in_block("who's there?")
+print_in_block("interrupted cow")
+print_in_block("interrupted--")
+print_in_block("MOO!")
